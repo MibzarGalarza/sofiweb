f1cd41a377053f0a6557177c028c1066e1390257
Erickc404 <erick.morales.isw@gmail.com>
2024-06-18 20:08:33 -0600

    se agreg[o pagina de locacion

--- src/components/mapa.jsx
+++ src/components/mapa.jsx
@@ -1,2 +1,12 @@
 import React from 'react'
+import GoogleMaps from 
 
+const mapa = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default mapa
--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
-import mapa from '../components/mapa'
+import NavBar from "../components/NavBar"
+import mapa from "../components/mapa"
 
+const location = () => {
+    return (
 
+    <div className='grid h-screen w-auto max-h-screen overflow-y-scroll'> 
+      <NavBar/>
+      <>
+        <main className='mt-24 h-screen w-screen bg-slate-200'>
+            <button className='btn bn bg-red-600'>ok</button>
+        </main>
+      </>
+    </div>
+    )
+}
 
+export default location
