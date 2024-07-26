e0fab592d126a815f65fad949419a3a9a7d21ca9
José Manuel <gjosemanuel375@gmail.com>
2024-07-25 19:04:49 -0600

    GALERIA AGREGADA AL NAVBAR y estilos  cambiados

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,15 @@ const NavBar = () => {
     return (
         <header className='absolute top-0 flex justify-center items-center p-5 h-24 bg-opacity-15 bg-[#D9D9D9] w-screen'>
             <nav className='flex items-center space-x-4 text-black text-xl font-serif'>
-                <a className='bg-white bg-opacity-40 p-3 rounded-2xl' href="/Servicios">SERVICIOS</a>
-                <a className='bg-white bg-opacity-40 p-3 rounded-2xl' href="/Portafolio">PORTAFOLIO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Servicios">SERVICIOS</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Portafolio">PORTAFOLIO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Galeria">GALERIA</a>
                 <a href="/" className=''>
                     <img className='h-20 w-auto' src={logoSOFI} alt="LOGOTIPO" />
                 </a>
-                <a className='bg-white bg-opacity-40 p-3 rounded-2xl' href="/Mobiliario">MOBILIARIO</a>
-                <a className='bg-white bg-opacity-40 p-3 rounded-2xl' href="/Contacto">CONTACTO</a>
-                <a className='bg-white bg-opacity-40 p-3 rounded-2xl' href="/Ubicacion">VISÍTANOS</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Mobiliario">MOBILIARIO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Contacto">CONTACTO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Ubicacion">VISÍTANOS</a>
             </nav>
         </header>
     )
