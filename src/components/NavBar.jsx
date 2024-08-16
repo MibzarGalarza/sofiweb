df3845b37c417536234d6f674a1975f26ff93d0b
Erickc404 <erick.morales.isw@gmail.com>
2024-08-16 14:17:32 -0600

    volvimos a donde mismo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from 'react'
 
 const NavBar = () => {
-  const logoSOFI = "./assets/Erick WEB/logosofi.png";
+    const logoSOFI = "./assets/Erick WEB/logosofi.png"
 
-  return (
-    <header className='absolute top-0 flex justify-center items-center p-5 h-24 bg-opacity-15 bg-[#D9D9D9] w-screen'>
-      <nav className='flex items-center space-x-4 text-black text-xl font-serif'>
-        <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href={`${process.env.PUBLIC_URL}/Servicios`}>SERVICIOS</a>
-        <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href={`${process.env.PUBLIC_URL}/Portafolio`}>PORTAFOLIO</a>
-        <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href={`${process.env.PUBLIC_URL}/Galeria`}>GALERIA</a>
-        <a href={`${process.env.PUBLIC_URL}/`} className=''>
-          <img className='h-20 w-auto' src={logoSOFI} alt="LOGOTIPO" />
-        </a>
-        <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href={`${process.env.PUBLIC_URL}/Mobiliario`}>MOBILIARIO</a>
-        <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href={`${process.env.PUBLIC_URL}/Contacto`}>CONTACTO</a>
-        <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href={`${process.env.PUBLIC_URL}/Ubicacion`}>VISÍTANOS</a>
-      </nav>
-    </header>
-  );
-};
+    return (
+        <header className='absolute top-0 flex justify-center items-center p-5 h-24 bg-opacity-15 bg-[#D9D9D9] w-screen'>
+            <nav className='flex items-center space-x-4 text-black text-xl font-serif'>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Servicios">SERVICIOS</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Portafolio">PORTAFOLIO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Galeria">GALERIA</a>
+                <a href="/" className=''>
+                    <img className='h-20 w-auto' src={logoSOFI} alt="LOGOTIPO" />
+                </a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Mobiliario">MOBILIARIO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Contacto">CONTACTO</a>
+                <a className='bg-white bg-opacity-40 p-2 text-base rounded-2xl' href="/Ubicacion">VISÍTANOS</a>
+            </nav>
+        </header>
+    )
+}
 
-export default NavBar;
+export default NavBar
